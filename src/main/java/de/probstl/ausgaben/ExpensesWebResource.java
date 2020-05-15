439baf8df7940142b0b211e475b3cbb6ad69b42a
Florian Probst <florian.probst@posteo.de>
2020-05-16 00:45:13 +0200

    added export url for downloading an csv file

--- src/main/java/de/probstl/ausgaben/ExpensesWebResource.java
+++ src/main/java/de/probstl/ausgaben/ExpensesWebResource.java
@@ -1,5 +1,9 @@
 package de.probstl.ausgaben;
 
+import java.io.PrintWriter;
+import java.text.DecimalFormat;
+import java.text.MessageFormat;
+import java.text.NumberFormat;
 import java.time.DayOfWeek;
 import java.time.Instant;
 import java.time.LocalDate;
@@ -21,10 +25,12 @@ import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
@@ -40,6 +46,7 @@ import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
 
 import de.probstl.ausgaben.data.Expense;
+import de.probstl.ausgaben.data.ExpensesRequest;
 import de.probstl.ausgaben.data.HomeForm;
 import de.probstl.ausgaben.mail.CityInfo;
 import de.probstl.ausgaben.mail.MailInfo;
@@ -110,6 +117,7 @@ public class ExpensesWebResource implements WebMvcConfigurer {
 
 		final Collection<String> monthList = new ArrayList<String>();
 
+		// Here's when we started to collect the expenses
 		LocalDate startDate = LocalDate.of(2020, Month.APRIL, 1);
 
 		String month = null;
@@ -142,45 +150,116 @@ public class ExpensesWebResource implements WebMvcConfigurer {
 	@GetMapping("/view/{month}/{year}")
 	public String showMonth(@PathVariable(name = "month") String month, @PathVariable(name = "year") String year,
 			Model model, Authentication auth) {
+		final ExpensesRequest request = ExpensesRequest.forMonth(month, year);
+		loadData(request, model, auth);
+		return "email";
+	}
 
-		LocalDateTime begin;
+	/**
+	 * Allows the export of a month into a CSV file
+	 * 
+	 * @param month    The month that should be exported
+	 * @param year     The year
+	 * @param response The response to write the CSV data
+	 * @param auth     Auth for choosing the collection
+	 * @throws Exception
+	 */
+	@GetMapping("/export/{month}/{year}")
+	public void exportMonth(@PathVariable(name = "month") String month, @PathVariable(name = "year") String year,
+			HttpServletResponse response, Authentication auth) throws Exception {
 
-		try {
-			begin = LocalDateTime.of(LocalDate.of(Integer.parseInt(year), Integer.parseInt(month), 1),
-					LocalTime.of(0, 0, 0));
-		} catch (NumberFormatException e) {
-			LOG.warn("Illegal month/year in requeset", e);
-			begin = LocalDateTime.of(LocalDate.now().with(TemporalAdjusters.firstDayOfMonth()), LocalTime.of(0, 0, 0));
-		}
-
-		Date beginDate = Date.from(ZonedDateTime.of(begin, ZoneId.systemDefault()).toInstant());
-		model.addAttribute("beginDate", beginDate);
-
-		LocalDateTime end = begin.plusMonths(1);
-		Date endDate = Date.from(ZonedDateTime.of(end, ZoneId.systemDefault()).toInstant());
-		model.addAttribute("endDate", endDate);
-		model.addAttribute("currency", "€");
-
-		final MailInfo mailInfo = new MailInfo(beginDate, endDate);
+		final ExpensesRequest request = ExpensesRequest.forMonth(month, year);
 		final Map<String, CityInfo> cityMapping;
 
 		Optional<? extends GrantedAuthority> authority = auth.getAuthorities().stream().findFirst();
 		if (authority.isPresent()) {
 			String collection = authority.get().getAuthority();
 			LOG.info("Using collection {} for user {}", collection, auth.getName());
-			cityMapping = findExpensesByDate(collection, beginDate, endDate);
+			cityMapping = findExpensesByDate(collection, request);
 		} else {
 			LOG.warn("No authority for user {} found. Showing empty data!", auth.getName());
 			cityMapping = Collections.emptyMap();
 		}
 
+		// set file name and content type
+		String filename = MessageFormat.format("expenses_{0,number,00}-{1,number,0000}.csv", Integer.valueOf(month),
+				Integer.valueOf(year));
+
+		response.setContentType("text/csv; charset=UTF-8");
+		response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"");
+
+		final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
+		final String lineSeparator = System.getProperty("line.separator");
+		final PrintWriter printWriter = response.getWriter();
+
+		for (CityInfo cityInfo : cityMapping.values()) {
+			for (Expense expense : cityInfo.getExpenses()) {
+				ZonedDateTime dt = expense.getTimestamp().toInstant().atZone(ZoneId.systemDefault());
+				printWriter.append(formatter.format(dt));
+				printWriter.append(",");
+				printWriter.append(quote(expense.getMessage()));
+				printWriter.append(",,");
+				printWriter.append(MessageFormat.format("\"{0,number,0.00} €\"", expense.getAmountDouble()));
+				printWriter.append(lineSeparator);
+			}
+		}
+		printWriter.flush();
+	}
+
+	/**
+	 * Quotes the message. If there area double quotes they are escaped by adding
+	 * additional double quotes to escape them. If there are comma in the message,
+	 * the message is surrounded by double quotes
+	 * 
+	 * @param message The message that should be quoted
+	 * @return Quoted message
+	 */
+	private String quote(String message) {
+
+		String toReturn = message + "\"BUG\" for testing";
+
+		boolean hasDoubleQuotes = toReturn.indexOf("\"") >= 0;
+		boolean hasComma = message.indexOf(",") >= 0;
+		if (hasDoubleQuotes) {
+			toReturn = toReturn.replaceAll("\"", "\"\"");
+		}
+		if (hasComma) {
+			toReturn = "\"" + toReturn + "\"";
+		}
+
+		return toReturn;
+	}
+
+	/**
+	 * Load all expenses during the given request interval and fill the given model
+	 * with data
+	 * 
+	 * @param request The request providing begin and end dates
+	 * @param model   The model that must be filled with data
+	 * @param auth    Authentication for choosing the collection
+	 */
+	private void loadData(ExpensesRequest request, Model model, Authentication auth) {
+		final Map<String, CityInfo> cityMapping;
+
+		Optional<? extends GrantedAuthority> authority = auth.getAuthorities().stream().findFirst();
+		if (authority.isPresent()) {
+			String collection = authority.get().getAuthority();
+			LOG.info("Using collection {} for user {}", collection, auth.getName());
+			cityMapping = findExpensesByDate(collection, request);
+		} else {
+			LOG.warn("No authority for user {} found. Showing empty data!", auth.getName());
+			cityMapping = Collections.emptyMap();
+		}
+
+		final MailInfo mailInfo = new MailInfo(request.getBeginDate(), request.getEndDate());
 		cityMapping.values().stream().forEach(x -> mailInfo.addCityInfo(x));
 
+		model.addAttribute("beginDate", request.getBeginDate());
+		model.addAttribute("endDate", request.getEndDate());
 		model.addAttribute("cities", mailInfo.getCityList());
 		model.addAttribute("sumShops", mailInfo.getSumShops());
 		model.addAttribute("sum", mailInfo.getSum());
-
-		return "email";
+		model.addAttribute("currency", "€");
 	}
 
 	/**
@@ -191,37 +270,8 @@ public class ExpensesWebResource implements WebMvcConfigurer {
 	 */
 	@GetMapping("/view/currentWeek")
 	public String showWeek(Model model, Authentication auth) {
-
-		LocalDate monday = LocalDate.now().with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
-
-		LocalDateTime begin = LocalDateTime.of(monday, LocalTime.of(0, 0, 0));
-		Date beginDate = Date.from(ZonedDateTime.of(begin, ZoneId.systemDefault()).toInstant());
-		model.addAttribute("beginDate", beginDate);
-
-		LocalDateTime end = begin.plusDays(7);
-		Date endDate = Date.from(ZonedDateTime.of(end, ZoneId.systemDefault()).toInstant());
-		model.addAttribute("endDate", endDate);
-		model.addAttribute("currency", "€");
-
-		final MailInfo mailInfo = new MailInfo(beginDate, endDate);
-		final Map<String, CityInfo> cityMapping;
-
-		Optional<? extends GrantedAuthority> authority = auth.getAuthorities().stream().findFirst();
-		if (authority.isPresent()) {
-			String collection = authority.get().getAuthority();
-			LOG.info("Using collection {} for user {}", collection, auth.getName());
-			cityMapping = findExpensesByDate(collection, beginDate, endDate);
-		} else {
-			LOG.warn("No authority for user {} found. Showing empty data!", auth.getName());
-			cityMapping = Collections.emptyMap();
-		}
-
-		cityMapping.values().stream().forEach(x -> mailInfo.addCityInfo(x));
-
-		model.addAttribute("cities", mailInfo.getCityList());
-		model.addAttribute("sumShops", mailInfo.getSumShops());
-		model.addAttribute("sum", mailInfo.getSum());
-
+		final ExpensesRequest request = ExpensesRequest.forCurrentWeek();
+		loadData(request, model, auth);
 		return "email";
 	}
 
@@ -232,10 +282,11 @@ public class ExpensesWebResource implements WebMvcConfigurer {
 	 * @param endDate   End date
 	 * @return Map of cities and expenses by city
 	 */
-	private Map<String, CityInfo> findExpensesByDate(String collection, Date beginDate, Date endDate) {
+	private Map<String, CityInfo> findExpensesByDate(String collection, ExpensesRequest request) {
 		final Map<String, CityInfo> cityMapping = new HashMap<>();
 
-		LOG.info("Find expenses from {} to {} in collection {}", beginDate, endDate, collection);
+		LOG.info("Find expenses from {} to {} in collection {}", request.getBeginDate(), request.getEndDate(),
+				collection);
 
 		ZoneId systemZone = ZoneId.systemDefault();
 		ZoneId homeZone = ZoneId.of("Europe/Berlin");
@@ -244,8 +295,8 @@ public class ExpensesWebResource implements WebMvcConfigurer {
 
 		try {
 			ApiFuture<QuerySnapshot> future = m_FirestoreService.getService().collection(collection)
-					.whereGreaterThan("timestamp", beginDate).whereLessThan("timestamp", endDate).orderBy("timestamp")
-					.get();
+					.whereGreaterThan("timestamp", request.getBeginDate())
+					.whereLessThan("timestamp", request.getEndDate()).orderBy("timestamp").get();
 
 			QuerySnapshot queryResult = future.get();
 			LOG.info("Read time {}", queryResult.getReadTime());
---
+++ src/main/java/de/probstl/ausgaben/data/ExpensesRequest.java
@@ -0,0 +1,95 @@
+package de.probstl.ausgaben.data;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Class for creating request intervals with begin and end dates
+ */
+public class ExpensesRequest {
+
+	/** The logger */
+	private static final Logger LOG = LoggerFactory.getLogger(ExpensesRequest.class);
+
+	/** Begin date for the expenses request interval */
+	private final Date m_BeginDate;
+
+	/** End date for the expenses request interval */
+	private final Date m_EndDate;
+
+	/**
+	 * Create a new request based on the given month
+	 * 
+	 * @param month The month as integer string
+	 * @param year  The year as integer string
+	 * @return New expenses request with the given begin- and end dates
+	 */
+	public static ExpensesRequest forMonth(String month, String year) {
+		LocalDateTime begin;
+		try {
+			begin = LocalDateTime.of(LocalDate.of(Integer.parseInt(year), Integer.parseInt(month), 1),
+					LocalTime.of(0, 0, 0));
+		} catch (NumberFormatException e) {
+			LOG.warn("Illegal month/year in requeset", e);
+			begin = LocalDateTime.of(LocalDate.now().with(TemporalAdjusters.firstDayOfMonth()), LocalTime.of(0, 0, 0));
+		}
+
+		Date beginDate = Date.from(ZonedDateTime.of(begin, ZoneId.systemDefault()).toInstant());
+
+		LocalDateTime end = begin.plusMonths(1);
+		Date endDate = Date.from(ZonedDateTime.of(end, ZoneId.systemDefault()).toInstant());
+
+		return new ExpensesRequest(beginDate, endDate);
+	}
+
+	/**
+	 * Create a new request based on current week
+	 * 
+	 * @return New expenses request with the given begin- and end dates
+	 */
+	public static ExpensesRequest forCurrentWeek() {
+		LocalDate monday = LocalDate.now().with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
+
+		LocalDateTime begin = LocalDateTime.of(monday, LocalTime.of(0, 0, 0));
+		Date beginDate = Date.from(ZonedDateTime.of(begin, ZoneId.systemDefault()).toInstant());
+
+		LocalDateTime end = begin.plusDays(7);
+		Date endDate = Date.from(ZonedDateTime.of(end, ZoneId.systemDefault()).toInstant());
+
+		return new ExpensesRequest(beginDate, endDate);
+	}
+
+	/**
+	 * Private constructor must provide begin- and end dates
+	 * 
+	 * @param begin Begin date of the request interval
+	 * @param end   End date of the request interval
+	 */
+	private ExpensesRequest(Date begin, Date end) {
+		m_BeginDate = begin;
+		m_EndDate = end;
+	}
+
+	/**
+	 * @return the m_BeginDate
+	 */
+	public Date getBeginDate() {
+		return m_BeginDate;
+	}
+
+	/**
+	 * @return the m_EndDate
+	 */
+	public Date getEndDate() {
+		return m_EndDate;
+	}
+}
