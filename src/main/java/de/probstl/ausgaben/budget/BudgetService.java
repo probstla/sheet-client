ff4f622e008b4d2da05cfc91034da571b932dd8a
Florian Probst <florian.probst@posteo.de>
2020-06-01 17:09:56 +0200

    Budget service working
    finished unit tests and implementation of the service

--- src/main/java/de/probstl/ausgaben/budget/BudgetService.java
+++ src/main/java/de/probstl/ausgaben/budget/BudgetService.java
@@ -2,14 +2,24 @@ package de.probstl.ausgaben.budget;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
 import org.springframework.util.ResourceUtils;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -17,6 +27,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.probstl.ausgaben.data.Expense;
 
+@Component
 public class BudgetService {
 
 	/** Logger */
@@ -29,7 +40,66 @@ public class BudgetService {
 	 * @return A Map with the budget and the corresponding amount
 	 */
 	public Map<Budget, Double> createFromExpenses(Authentication auth, Collection<Expense> expenses) {
-		return null;
+
+		final Map<Budget, Double> toReturn = new TreeMap<Budget, Double>();
+
+		final Collection<Budget> budgets = readDefinition(auth.getName());
+		for (Budget budget : budgets) {
+			Double sum = findMatching(budget, expenses).stream().mapToDouble(x -> x.getAmountDouble()).sum();
+			toReturn.put(budget, sum);
+		}
+
+		return toReturn;
+	}
+
+	/**
+	 * Find the matching expenses by the given budget
+	 * 
+	 * @param budget   the budget definition
+	 * @param expenses the found expenses that are checked against the budget
+	 * @return matching expenses, never <code>null</code>
+	 */
+	Collection<Expense> findMatching(Budget budget, Collection<Expense> expenses) {
+
+		Collection<Expense> filtered = expenses;
+
+		String regex = budget.getMessageRegex();
+		if (regex != null && !regex.trim().isEmpty()) {
+			Pattern pattern = null;
+			try {
+				pattern = Pattern.compile(regex);
+				filtered = findByRegex(pattern, expenses);
+			} catch (PatternSyntaxException e) {
+				m_Log.error("invalid pattern: " + regex, e);
+			}
+		}
+
+		if (budget.getShops() == null || budget.getShops().length == 0) {
+			return filtered; // Without shops just filter by regex
+		}
+
+		Set<String> shops = new HashSet<String>(Arrays.asList(budget.getShops()));
+		return filtered.stream().filter(x -> shops.contains(x.getShop())).collect(Collectors.toList());
+	}
+
+	/**
+	 * Find the expenses matching the given regular expression
+	 * 
+	 * @param pattern  The regular expression from the budget
+	 * @param expenses The expenses that are checked against the budget
+	 * @return matching expenses, never <code>null</code>
+	 */
+	private Collection<Expense> findByRegex(Pattern pattern, Collection<Expense> expenses) {
+
+		final Collection<Expense> toReturn = new ArrayList<Expense>();
+		for (Expense expense : expenses) {
+			Matcher matcher = pattern.matcher(expense.getMessage());
+			if (matcher.matches()) {
+				toReturn.add(expense);
+			}
+		}
+
+		return toReturn;
 	}
 
 	/**
--- src/test/java/de/probstl/ausgaben/budget/TestBudgetService.java
+++ src/test/java/de/probstl/ausgaben/budget/TestBudgetService.java
@@ -1,5 +1,11 @@
 package de.probstl.ausgaben.budget;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Optional;
 
@@ -7,6 +13,8 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import de.probstl.ausgaben.data.Expense;
 
 /**
  * Test for the budget service
@@ -19,12 +27,64 @@ public class TestBudgetService {
 	 */
 	@Test
 	public void testReadDefinition() {
-		BudgetService service = new BudgetService();
+		final BudgetService service = new BudgetService();
 		Collection<Budget> definition = service.readDefinition("flo");
-		Assert.assertNotNull(definition);
-		Assert.assertTrue(!definition.isEmpty());
-		
-		Optional<String> budget = definition.stream().map(x -> x.getName()).filter(x -> x.equalsIgnoreCase("Lebensmittel")).findFirst();
+		assertNotNull(definition);
+		assertTrue(!definition.isEmpty());
+
+		Optional<String> budget = definition.stream().map(x -> x.getName())
+				.filter(x -> x.equalsIgnoreCase("Lebensmittel")).findFirst();
 		Assert.assertTrue(budget.isPresent());
+	}
+
+	/**
+	 * Test for detecting expenses by regex
+	 */
+	@Test
+	public void testRegex() {
+
+		Budget budget = new Budget();
+		budget.setName("Auto");
+		budget.setMessageRegex(".*Auto.*");
+
+		Collection<Expense> expenses = new ArrayList<Expense>();
+		Expense e1 = new Expense();
+		e1.setMessage("Autowaschmittel");
+		expenses.add(e1);
+
+		final BudgetService service = new BudgetService();
+		Collection<Expense> matching = service.findMatching(budget, expenses);
+		assertFalse(matching.isEmpty());
+	}
+
+	/**
+	 * Testcase for finding expenses by a given shop
+	 */
+	@Test
+	public void testShops() {
+		Budget budget = new Budget();
+		budget.setName("Bäcker");
+		budget.setShops(new String[] { "Betz", "Wackerl", "Mareis", "Frühmorgen" });
+
+		Collection<Expense> expenses = new ArrayList<Expense>();
+		Expense e1 = new Expense();
+		e1.setShop("Betz");
+		e1.setAmountDouble(2.0);
+		expenses.add(e1);
+
+		e1 = new Expense();
+		e1.setShop("EDEKA");
+		e1.setAmountDouble(2.0);
+		expenses.add(e1);
+
+		e1 = new Expense();
+		e1.setShop("Wackerl");
+		e1.setAmountDouble(2.0);
+		expenses.add(e1);
+
+		final BudgetService service = new BudgetService();
+		Collection<Expense> matching = service.findMatching(budget, expenses);
+		assertFalse(matching.isEmpty());
+		assertEquals(4.0, matching.stream().mapToDouble(x -> x.getAmountDouble()).sum());
 	}
 }
