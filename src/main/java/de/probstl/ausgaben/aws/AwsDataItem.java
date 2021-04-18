2885683a82db5edb5f2be82f79b3c5348d126851
Florian Probst <florian.probst@posteo.de>
2021-04-18 18:06:37 +0200

    Modified AWS JSON format

--- src/main/java/de/probstl/ausgaben/aws/AwsDataItem.java
+++ src/main/java/de/probstl/ausgaben/aws/AwsDataItem.java
@@ -2,6 +2,8 @@ package de.probstl.ausgaben.aws;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
@@ -22,12 +24,14 @@ public class AwsDataItem {
     private String city;
 
     /** Timestamp of the expense */
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SS'Z'", timezone = "UTC")
     private Date timestamp;
 
     /** Amount of the expense */
     private Double amount;
 
     /** The budget the expense has been assigned to */
+    @JsonIgnore
     private String budget;
 
     /** Was the expense cash or card */
